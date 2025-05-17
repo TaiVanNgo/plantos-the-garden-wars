de01bb08d16672d8b44466c3b7672b574e9bdb7a
TDuong04 <Facebookduongtalon@gmail.com>
2025-05-18 00:35:24 +0700

    fix!: windows and macOs compatible make file

--- src/kernel.c
+++ src/kernel.c
@@ -5,83 +5,83 @@
 #include "../include/cmd.h"
 #include "../include/video.h"
 #include "../include/utils.h"
-#include "../include/background.h"
+#include "../assets/backgrounds/background.h"
 //#include "background.c"
   #define TASK1
  // #define TASK2_VID
 
-void display_team_members(int show_bg)
-{
-    // if show background
-    if (show_bg)
-    {
-        draw_image(BACKGROUND, 0, 0, BACKGROUND_WIDTH, BACKGROUND_HEIGHT);
-    }
+// void display_team_members(int show_bg)
+// {
+//     // if show background
+//     if (show_bg)
+//     {
+//         draw_image(BACKGROUND, 0, 0, BACKGROUND_WIDTH, BACKGROUND_HEIGHT, 0);
+//     }
 
-    // Title
-    draw_string(260, 20, "EMBEDDED SYSTEMS", RED, 2);
-    draw_string(150, 50, "OPERATING SYSTEMS AND INTERFACING", RED, 2);
+//     // Title
+//     draw_string(260, 20, "EMBEDDED SYSTEMS", RED, 2);
+//     draw_string(150, 50, "OPERATING SYSTEMS AND INTERFACING", RED, 2);
 
-    // Member 1
+//     // Member 1
 
-    draw_circle(160, 80, 8, GREEN_YELLOW, 1);
-    draw_string(200, 75, "NGUYEN PHAM ANH THU (S3926793)", GREEN_YELLOW, 2);
+//     draw_circle(160, 80, 8, GREEN_YELLOW, 1);
+//     draw_string(200, 75, "NGUYEN PHAM ANH THU (S3926793)", GREEN_YELLOW, 2);
 
-    // Member 2
-    draw_circle(160, 100, 8, BRIGHT_BLUE, 1);
-    draw_string(200, 95, "HUYNH TAN PHAT (S3929218)", BRIGHT_BLUE, 2);
+//     // Member 2
+//     draw_circle(160, 100, 8, BRIGHT_BLUE, 1);
+//     draw_string(200, 95, "HUYNH TAN PHAT (S3929218)", BRIGHT_BLUE, 2);
 
-    // Member 3
-    draw_circle(160, 120, 8, SALMON_RED, 1);
-    draw_string(200, 115, "HUYNH THAI DUONG (S3978955)", SALMON_RED, 2);
+//     // Member 3
+//     draw_circle(160, 120, 8, SALMON_RED, 1);
+//     draw_string(200, 115, "HUYNH THAI DUONG (S3978955)", SALMON_RED, 2);
 
-    // Member 4
-    draw_circle(160, 140, 8, THISTLE, 1);
-    draw_string(200, 135, "NGO VAN TAI (S3974892)", THISTLE, 2);
+//     // Member 4
+//     draw_circle(160, 140, 8, THISTLE, 1);
+//     draw_string(200, 135, "NGO VAN TAI (S3974892)", THISTLE, 2);
 
-    // Pulse circle animation
-    int pulse = 0;
-    int pulse_dir = 1;
+//     // Pulse circle animation
+//     int pulse = 0;
+//     int pulse_dir = 1;
 
-    while (1)
-    {
-        // Pulse animation
-        if (pulse_dir > 0)
-        {
-            pulse++;
-            if (pulse >= 10)
-                pulse_dir = -1;
-        }
-        else
-        {
-            pulse--;
-            if (pulse <= 4)
-                pulse_dir = 1;
-        }
-        // Clear previous left circle
-        draw_circle(210, 20, 15, PULSE_CIRCLE_COLOR, 1);
-        // Draw new pulse left circle
-        draw_circle(210, 20, pulse, WHITE, 1);
+//     while (1)
+//     {
+//         // Pulse animation
+//         if (pulse_dir > 0)
+//         {
+//             pulse++;
+//             if (pulse >= 10)
+//                 pulse_dir = -1;
+//         }
+//         else
+//         {
+//             pulse--;
+//             if (pulse <= 4)
+//                 pulse_dir = 1;
+//         }
+//         // Clear previous left circle
+//         draw_circle(210, 20, 15, PULSE_CIRCLE_COLOR, 1);
+//         // Draw new pulse left circle
+//         draw_circle(210, 20, pulse, WHITE, 1);
 
-        // Clear previous right circle
-        draw_circle(550, 20, 15, PULSE_CIRCLE_COLOR, 1);
-        // Draw new pulse right circle
-        draw_circle(550, 20, pulse, WHITE, 1);
+//         // Clear previous right circle
+//         draw_circle(550, 20, 15, PULSE_CIRCLE_COLOR, 1);
+//         // Draw new pulse right circle
+//         draw_circle(550, 20, pulse, WHITE, 1);
 
-        // Process UART input
-        char c = getUart();
+//         // Process UART input
+//         char c = getUart();
 
-        if (c == 'q')
-        {
-            uart_puts("Exit displaying team member!\n");
-            clear_screen();
-            return;
-        }
+//         if (c == 'q')
+//         {
+//             uart_puts("Exit displaying team member!\n");
+//             clear_screen();
+//             return;
+//         }
 
-        // Short delay for animation
-        delay_ms(500);
-    }
-}
+//         // Short delay for animation
+//         delay_ms(500);
+//     }
+// }
 
 #ifdef TASK1
 void main()
