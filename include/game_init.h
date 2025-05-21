151d447924164016208450cacd4d4c07d4d328f0
TaiVanNgo <vantaingo.056@gmail.com>
2025-05-21 18:01:00 +0700

    feat(game_main): main loop

--- include/game_init.h
+++ include/game_init.h
@@ -5,6 +5,24 @@
 #include "zombies.h"
 #include "utils.h"
 
+// Enum for game states
+typedef enum
+{
+  GAME_MENU,
+  GAME_PLAYING,
+  GAME_PAUSED,
+  GAME_OVER
+} GAME_STATE;
+
+// Main game state structure
+typedef struct
+{
+  GAME_STATE state;
+  int score;
+  int round;
+} GameState;
+
 void game_init();
 void game_start();
+
 #endif
