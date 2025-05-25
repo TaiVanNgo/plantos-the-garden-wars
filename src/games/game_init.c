5f4ee999632f3674b6196a1a56e2fa931cd45a54
Thu Nguyen <101914949+npat273@users.noreply.github.com>
2025-05-26 01:09:19 +0700

    feat: trigger sun count warning when placing plants instead of previewing

--- src/games/game_init.c
+++ src/games/game_init.c
@@ -525,17 +525,6 @@ void handle_plant_selection(int plant_type, int frame_counter)
     // Check if plant is on cooldown
     if (plant_type >= 1 && plant_type <= 5)
     {
-        int cost = get_plant_cost(plant_type);
-        if (game.sun_count < cost) {
-            uart_puts("Not enough sun! Plant cost: ");
-            uart_dec(cost);
-            uart_puts(", Available sun: ");
-            uart_dec(game.sun_count);
-            uart_puts("\n");
-
-            trigger_insufficient_sun_warning(frame_counter);
-            return;
-        }
 
         if (is_plant_on_cooldown(plant_type))
         {
@@ -750,11 +739,7 @@ void handle_enter_key(int frame_counter)
     int plant_cost = get_plant_cost(select_state.current_plant);
     if (game.sun_count < plant_cost)
     {
-        uart_puts("Not enough sun! Plant cost: ");
-        uart_dec(plant_cost);
-        uart_puts(", Available sun: ");
-        uart_dec(game.sun_count);
-        uart_puts("\n");
+        trigger_insufficient_sun_warning(frame_counter);
         return;
     }
 
