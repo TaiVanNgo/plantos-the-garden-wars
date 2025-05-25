3df31e887bd5afda4f8115ffdb663110946e7d0f
phatgg221 <129394719+phatgg221@users.noreply.github.com>
2025-05-25 15:32:01 +0700

    fix the same gird bug

--- src/games/game_init.c
+++ src/games/game_init.c
@@ -551,6 +551,9 @@ void handle_plant_selection(int plant_type)
         int taken = check_clear() ? 1 : 0;
         clear_plant_from_background(prev_col, prev_row, 0, taken);
         // clear_plant_from_background(select_state.col, select_state.row, 0, taken);
+        if(check_occupied()){
+            clear_plant_from_background(select_state.col, select_state.row, 0, taken);
+        }
         place_plant_on_background(select_state.current_plant, select_state.col, select_state.row, simulated_background);
         // grid_to_pixel(select_state.col, select_state.row, &x, &y);
         restore_background_area(x, y, GRID_COL_WIDTH, GRID_ROW_HEIGHT, 0);
