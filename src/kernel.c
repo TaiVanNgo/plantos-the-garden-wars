80695af115a901e44efeb47e2b769b81be56c4bc
Thu Nguyen <101914949+npat273@users.noreply.github.com>
2025-05-27 03:30:18 +0700

    refactor: remove unused code

--- src/kernel.c
+++ src/kernel.c
@@ -1,20 +1,19 @@
-#include "../include/uart0.h"
-#include "../include/uart1.h"
-#include "../include/mbox.h"
-#include "../include/framebf.h"
-#include "../include/cmd.h"
-#include "../include/video.h"
-#include "../include/utils.h"
-#include "../include/zombies.h"
-#include "../include/plants.h"
-#include "../include/bullet.h"
-#include "../include/game_init.h"
 #include "../assets/backgrounds/background.h"
 #include "../assets/backgrounds/garden.h"
+#include "../include/bullet.h"
+#include "../include/cmd.h"
+#include "../include/framebf.h"
+#include "../include/game_init.h"
+#include "../include/mbox.h"
+#include "../include/plants.h"
+#include "../include/uart0.h"
+#include "../include/uart1.h"
+#include "../include/utils.h"
+#include "../include/video.h"
+#include "../include/zombies.h"
 
 #if defined(ALL_INIT)
-void main()
-{
+void main() {
     // Initialize UART and framebuffer
     uart_init();
     framebf_init();
@@ -34,14 +33,12 @@ void main()
     uart_puts(PROMPT);
 
     // Main CLI loop
-    while (1)
-    {
+    while (1) {
         cli();
     }
 }
 #elif defined(CLI_INIT)
-void main()
-{
+void main() {
     // Initialize UART for CLI
     uart_init();
 
@@ -58,15 +55,13 @@ void main()
     uart_puts(PROMPT);
 
     // Main loop - handle both CLI and game input
-    while (1)
-    {
+    while (1) {
         // Process CLI commands
         cli();
     }
 }
 #elif defined(VID_INIT)
-void main()
-{
+void main() {
     // Set up the serial console
     uart_init();
 
@@ -78,54 +73,28 @@ void main()
     play_video(&vid, 80, 120, vid.total_frames);
 
     // Run CLI
-    while (1)
-    {
+    while (1) {
         char c = uart_getc();
         uart_sendc(c);
 
-        if (c == 'c')
-        {
+        if (c == 'c') {
             play_video(&vid, 80, 120, vid.total_frames);
         }
     }
 }
 #elif defined(GAME_INIT)
-void main()
-{
+void main() {
     uart_init();
     framebf_init();
     game_main();
 
-    while (1)
-    {
+    while (1) {
         char c = uart_getc();
         uart_dec(c);
     }
 }
-#elif defined(PLANT_INIT)
-void main()
-{
-    uart_init();
-    framebf_init();
-
-    // Draw the garden background
-    draw_image(GARDEN, 0, 0, BACKGROUND_WIDTH, BACKGROUND_HEIGHT, 0);
-
-    draw_grid();
-
-    fill_plant_grid();
-
-    // Main loop
-    while (1)
-    {
-        char c = getUart();
-        uart_sendc(c);
-    }
-}
 #elif defined(TEAM_INIT)
-void main()
-{
-
+void main() {
     uart_init();
     framebf_init();
     wipe_transition();
