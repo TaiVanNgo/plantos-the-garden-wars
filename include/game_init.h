f692a9bcc044f8d875dc7e1412682c97685b06e2
Thu Nguyen <101914949+npat273@users.noreply.github.com>
2025-05-27 12:19:29 +0700

    refactor: update game_init.h

--- include/game_init.h
+++ include/game_init.h
@@ -1,19 +1,19 @@
 #ifndef GAME_INIT_H
 #define GAME_INIT_H
 
-#include "../assets/selection/selection.h"
 #include "../assets/backgrounds/garden.h"
-#include "buttons.h"
+#include "../assets/selection/selection.h"
 #include "bullet.h"
-#include "zombies.h"
-#include "utils.h"
+#include "buttons.h"
+#include "cooldown.h"
+#include "framebf.h"
+#include "grid.h"
+#include "plants.h"
+#include "sun.h"
 #include "uart0.h"
 #include "uart1.h"
-#include "framebf.h"
-#include "plants.h"
-#include "grid.h"
-#include "sun.h"
-#include "cooldown.h"
+#include "utils.h"
+#include "zombies.h"
 
 #define ROWS 4
 #define COLS 9
@@ -21,47 +21,45 @@
 #define ZOMBIE_KILL_REWARD 10
 
 // Enum for game states
-typedef enum
-{
-  GAME_MENU,
-  GAME_DIFFICULTY,
-  GAME_PLAYING,
-  GAME_QUIT,
-  GAME_VICTORY,
-  GAME_OVER
+typedef enum {
+    GAME_MENU,
+    GAME_DIFFICULTY,
+    GAME_PLAYING,
+    GAME_QUIT,
+    GAME_VICTORY,
+    GAME_OVER
 } GAME_STATE;
 
-typedef enum
-{
-  LEVEL_EASY_ENUM,
-  LEVEL_MEDIUM_ENUM,
-  LEVEL_HARD_ENUM,
+typedef enum {
+    LEVEL_EASY_ENUM,
+    LEVEL_MEDIUM_ENUM,
+    LEVEL_HARD_ENUM,
 } LEVEL_DIFFICULTY;
 
 // Main game state structure
 typedef struct
 {
-  GAME_STATE state;
-  int score;
-  LEVEL_DIFFICULTY level;
-  int sun_count;
+    GAME_STATE state;
+    int score;
+    LEVEL_DIFFICULTY level;
+    int sun_count;
 } GameState;
 
 typedef struct
 {
-  int mode;          // 0 = card selection, 1 = grid placement
-  int selected_card; // currently selected plant card
-  int row;           // current row
-  int col;           // current col
-  int current_plant; // current selected plant type
+    int mode;           // 0 = card selection, 1 = grid placement
+    int selected_card;  // currently selected plant card
+    int row;            // current row
+    int col;            // current col
+    int current_plant;  // current selected plant type
 } SelectionState;
 
 typedef struct
 {
-  int zombie_count;                        // total zombies in a level
-  int zombie_types[MAX_ZOMBIES_PER_LEVEL]; // type of each zombie (normal, bucket, helmet)
-  int zombie_rows[MAX_ZOMBIES_PER_LEVEL];  // the row that zombie appears
-  int spawn_times[MAX_ZOMBIES_PER_LEVEL];  // spawn time for each zombie
+    int zombie_count;                         // total zombies in a level
+    int zombie_types[MAX_ZOMBIES_PER_LEVEL];  // type of each zombie (normal, bucket, helmet)
+    int zombie_rows[MAX_ZOMBIES_PER_LEVEL];   // the row that zombie appears
+    int spawn_times[MAX_ZOMBIES_PER_LEVEL];   // spawn time for each zombie
 } Level;
 
 // External global variables
@@ -78,11 +76,11 @@ int check_occupied();
 void handle_plant_selection(int plant_type, int current_frame);
 void handle_remove_plant();
 void handle_arrow_keys();
-void handle_enter_key();
+void handle_enter_key(int frame_counter);
 void victory_screen();
-int get_selection_current_plant(void);
-int get_selection_row(void);
-int get_selection_col(void);
 void set_zombie_level_config(int level, int zombie_types[10], int spawn_times[10]);
+void draw_cursor();
+void draw_all_plant_cooldowns();
+void draw_control_instructions();
 
 #endif
--- src/games/plants.c
+++ src/games/plants.c
@@ -1,10 +1,9 @@
 #include "../../include/plants.h"
+
+#include "../../include/game_init.h"
 #include "../../include/grid.h"
 #include "../../include/zombies.h"
-#include "../../include/game_init.h"
 
-// Add static counter array at the top of the file
-static int flame_counters[GRID_ROWS] = {0};
 int flame_start_frames[GRID_ROWS] = {0};
 int flame_active[GRID_ROWS] = {0};
 
@@ -15,9 +14,9 @@ const Plant default_sunflower = {
     .max_health = 50,
     .col = 0,
     .row = 0,
-    .attack_damage = 0, // Sunflowers don't attack
+    .attack_damage = 0,  // Sunflowers don't attack
     .cost = 50,
-    .attack_speed = 0, // Sunflowers don't attack
+    .attack_speed = 0,  // Sunflowers don't attack
 };
 
 // Default Peashooter
@@ -29,7 +28,7 @@ const Plant default_peashooter = {
     .row = 0,
     .attack_damage = 30,
     .cost = 100,
-    .attack_speed = 5, // Fires 5 peas per time unit
+    .attack_speed = 5,  // Fires 5 peas per time unit
 };
 
 // Default Frozen Peashooter
@@ -39,9 +38,9 @@ const Plant default_frozen_peashooter = {
     .max_health = 100,
     .col = 0,
     .row = 0,
-    .attack_damage = 20, // Slightly less damage than regular peashooter
-    .cost = 175,         // More expensive due to freezing ability
-    .attack_speed = 4,   // Fires 4 peas per time unit
+    .attack_damage = 20,  // Slightly less damage than regular peashooter
+    .cost = 175,          // More expensive due to freezing ability
+    .attack_speed = 4,    // Fires 4 peas per time unit
 };
 
 // Default Walnut
@@ -51,9 +50,9 @@ const Plant default_walnut = {
     .max_health = 250,
     .col = 0,
     .row = 0,
-    .attack_damage = 0, // Walnuts don't attack
+    .attack_damage = 0,  // Walnuts don't attack
     .cost = 50,
-    .attack_speed = 0, // Walnuts don't attack
+    .attack_speed = 0,  // Walnuts don't attack
 };
 
 // Default Chillies
@@ -63,35 +62,33 @@ const Plant default_chillies = {
     .max_health = 50,
     .col = 0,
     .row = 0,
-    .attack_damage = 255, // High damage as it's an explosive plant
+    .attack_damage = 255,  // High damage as it's an explosive plant
     .cost = 125,
     .attack_speed = 1,
 };
 
 // Function to create a new plant of the specified type
-Plant create_plant(uint8_t type, uint8_t col, uint8_t row)
-{
+Plant create_plant(uint8_t type, uint8_t col, uint8_t row) {
     Plant new_plant;
 
-    switch (type)
-    {
-    case PLANT_SUNFLOWER:
-        new_plant = default_sunflower;
-        break;
-    case PLANT_PEASHOOTER:
-        new_plant = default_peashooter;
-        break;
-    case PLANT_FROZEN_PEASHOOTER:
-        new_plant = default_frozen_peashooter;
-        break;
-    case PLANT_WALNUT:
-        new_plant = default_walnut;
-        break;
-    case PLANT_CHILLIES:
-        new_plant = default_chillies;
-        break;
-    default:
-        break;
+    switch (type) {
+        case PLANT_SUNFLOWER:
+            new_plant = default_sunflower;
+            break;
+        case PLANT_PEASHOOTER:
+            new_plant = default_peashooter;
+            break;
+        case PLANT_FROZEN_PEASHOOTER:
+            new_plant = default_frozen_peashooter;
+            break;
+        case PLANT_WALNUT:
+            new_plant = default_walnut;
+            break;
+        case PLANT_CHILLIES:
+            new_plant = default_chillies;
+            break;
+        default:
+            break;
     }
 
     // Set the grid position
@@ -101,10 +98,8 @@ Plant create_plant(uint8_t type, uint8_t col, uint8_t row)
     return new_plant;
 }
 
-void draw_plant(int plant_type, int col, int row)
-{
-    if (!is_valid_grid_position(col, row))
-    {
+void draw_plant(int plant_type, int col, int row) {
+    if (!is_valid_grid_position(col, row)) {
         return;
     }
 
@@ -122,45 +117,43 @@ void draw_plant(int plant_type, int col, int row)
 
     const unsigned int *plant_sprite;
 
-    switch (plant_type)
-    {
-    case PLANT_TYPE_PEASHOOTER:
-        plant_sprite = peashooter;
-        break;
-    case PLANT_TYPE_SUNFLOWER:
-        plant_sprite = sunflower;
-        break;
-    case PLANT_TYPE_SUNFLOWER_UNHAPPY:
-        plant_sprite = sunflower_unhappy;
-        break;
-    case PLANT_TYPE_FROZEN_PEASHOOTER:
-        plant_sprite = frozen_peashooter;
-        break;
-    case PLANT_TYPE_CHILLIES:
-        plant_sprite = chillies;
-        break;
-    case PLANT_TYPE_CHILLIES_UNHAPPY:
-        plant_sprite = chillies_unhappy;
-        break;
-    case PLANT_TYPE_WALLNUT:
-        plant_sprite = wallnut;
-        break;
-    case PLANT_TYPE_WALLNUT_UNHAPPY:
-        plant_sprite = wallnut_unhappy;
-        break;
-    case SHOVEL:
-        plant_sprite = shovel;
-        break;
-    default:
-        return;
+    switch (plant_type) {
+        case PLANT_TYPE_PEASHOOTER:
+            plant_sprite = peashooter;
+            break;
+        case PLANT_TYPE_SUNFLOWER:
+            plant_sprite = sunflower;
+            break;
+        case PLANT_TYPE_SUNFLOWER_UNHAPPY:
+            plant_sprite = sunflower_unhappy;
+            break;
+        case PLANT_TYPE_FROZEN_PEASHOOTER:
+            plant_sprite = frozen_peashooter;
+            break;
+        case PLANT_TYPE_CHILLIES:
+            plant_sprite = chillies;
+            break;
+        case PLANT_TYPE_CHILLIES_UNHAPPY:
+            plant_sprite = chillies_unhappy;
+            break;
+        case PLANT_TYPE_WALLNUT:
+            plant_sprite = wallnut;
+            break;
+        case PLANT_TYPE_WALLNUT_UNHAPPY:
+            plant_sprite = wallnut_unhappy;
+            break;
+        case SHOVEL:
+            plant_sprite = shovel;
+            break;
+        default:
+            return;
     }
 
     draw_image(plant_sprite, x, y, PLANT_WIDTH, PLANT_HEIGHT, 0);
 }
 
 // Fill the plant grid for testing purposes
-void fill_plant_grid(void)
-{
+void fill_plant_grid(void) {
     int plant_types[] = {
         PLANT_TYPE_PEASHOOTER,
         PLANT_TYPE_SUNFLOWER,
@@ -173,71 +166,64 @@ void fill_plant_grid(void)
     int num_types = sizeof(plant_types) / sizeof(plant_types[0]);
     int type_idx = 0;
 
-    for (int y = 0; y < GRID_ROWS; y++)
-    {
-        for (int x = 0; x < GRID_COLS; x++)
-        {
+    for (int y = 0; y < GRID_ROWS; y++) {
+        for (int x = 0; x < GRID_COLS; x++) {
             draw_plant(plant_types[type_idx], x, y);
             type_idx = (type_idx + 1) % num_types;
         }
     }
 }
 
-int get_plant_damage(int plant_type)
-{
-    switch (plant_type)
-    {
-    case PLANT_TYPE_PEASHOOTER:
-        return default_peashooter.attack_damage;
-    case PLANT_TYPE_FROZEN_PEASHOOTER:
-        return default_frozen_peashooter.attack_damage;
-    case PLANT_TYPE_CHILLIES:
-        return default_chillies.attack_damage;
-    default:
-        return 0;
+int get_plant_damage(int plant_type) {
+    switch (plant_type) {
+        case PLANT_TYPE_PEASHOOTER:
+            return default_peashooter.attack_damage;
+        case PLANT_TYPE_FROZEN_PEASHOOTER:
+            return default_frozen_peashooter.attack_damage;
+        case PLANT_TYPE_CHILLIES:
+            return default_chillies.attack_damage;
+        default:
+            return 0;
     }
 }
 
-void place_plant_on_background(int plant_type, int grid_col, int grid_row, unsigned int *sim_bg)
-{
-    if (plant_grid[grid_row][grid_col].type != 255)
-    {
+void place_plant_on_background(int plant_type, int grid_col, int grid_row, unsigned int *sim_bg) {
+    if (plant_grid[grid_row][grid_col].type != 255) {
         return;
     }
     const unsigned int *plant;
 
-    switch (plant_type)
-    {
-    case PLANT_TYPE_PEASHOOTER:
-        plant = peashooter;
-        break;
-    case PLANT_TYPE_SUNFLOWER:
-        plant = sunflower;
-        break;
-    case PLANT_TYPE_SUNFLOWER_UNHAPPY:
-        plant = sunflower_unhappy;
-        break;
-    case PLANT_TYPE_FROZEN_PEASHOOTER:
-        plant = frozen_peashooter;
-        break;
-    case PLANT_TYPE_CHILLIES:
-        plant = chillies;
-        break;
-    case PLANT_TYPE_CHILLIES_UNHAPPY:
-        plant = chillies_unhappy;
-        break;
-    case PLANT_TYPE_WALLNUT:
-        plant = wallnut;
-        break;
-    case PLANT_TYPE_WALLNUT_UNHAPPY:
-        plant = wallnut_unhappy;
-        break;
-    case SHOVEL:
-        plant = shovel;
-        break;
-    default:
-        uart_puts("Invalid plant type!\n");
-        return;
+    switch (plant_type) {
+        case PLANT_TYPE_PEASHOOTER:
+            plant = peashooter;
+            break;
+        case PLANT_TYPE_SUNFLOWER:
+            plant = sunflower;
+            break;
+        case PLANT_TYPE_SUNFLOWER_UNHAPPY:
+            plant = sunflower_unhappy;
+            break;
+        case PLANT_TYPE_FROZEN_PEASHOOTER:
+            plant = frozen_peashooter;
+            break;
+        case PLANT_TYPE_CHILLIES:
+            plant = chillies;
+            break;
+        case PLANT_TYPE_CHILLIES_UNHAPPY:
+            plant = chillies_unhappy;
+            break;
+        case PLANT_TYPE_WALLNUT:
+            plant = wallnut;
+            break;
+        case PLANT_TYPE_WALLNUT_UNHAPPY:
+            plant = wallnut_unhappy;
+            break;
+        case SHOVEL:
+            plant = shovel;
+            break;
+        default:
+            uart_puts("Invalid plant type!\n");
+            return;
     }
 
     int x, y;
@@ -255,8 +241,7 @@ void place_plant_on_background(int plant_type, int grid_col, int grid_row, unsig
     restore_background_area(x, y, PLANT_WIDTH, PLANT_HEIGHT, 0);
 }
 
-void draw_flames_on_row(int row)
-{
+void draw_flames_on_row(int row) {
     int x, y;
     grid_to_pixel(0, row, &x, &y);
     x = GRID_LEFT_MARGIN;
@@ -266,8 +251,7 @@ void draw_flames_on_row(int row)
     draw_image(FLAMES_EFFECT, x, y, FLAMES_EFFECT_WIDTH, FLAMES_EFFECT_HEIGHT, 0);
 }
 
-void clear_flames_on_row(int row)
-{
+void clear_flames_on_row(int row) {
     int x, y;
     grid_to_pixel(0, row, &x, &y);
     x = GRID_LEFT_MARGIN;
@@ -277,21 +261,16 @@ void clear_flames_on_row(int row)
     restore_background_area(x, y, FLAMES_EFFECT_WIDTH, FLAMES_EFFECT_HEIGHT, 0);
 }
 
-void chillies_detonate(int row, int current_frame)
-{
+void chillies_detonate(int row, int current_frame) {
     flame_active[row] = 1;
     flame_start_frames[row] = current_frame;
 }
 
-void update_flame_effects(int current_frame)
-{
+void update_flame_effects(int current_frame) {
     // Check if game is over or player has won
-    if (game.state == GAME_OVER || game.state == GAME_VICTORY)
-    {
-        for (int row = 0; row < GRID_ROWS; row++)
-        {
-            if (flame_active[row])
-            {
+    if (game.state == GAME_OVER || game.state == GAME_VICTORY) {
+        for (int row = 0; row < GRID_ROWS; row++) {
+            if (flame_active[row]) {
                 clear_flames_on_row(row);
                 flame_active[row] = 0;
             }
@@ -299,34 +278,27 @@ void update_flame_effects(int current_frame)
         return;
     }
 
-    for (int row = 0; row < GRID_ROWS; row++)
-    {
-        if (flame_active[row])
-        {
+    for (int row = 0; row < GRID_ROWS; row++) {
+        if (flame_active[row]) {
             // If 10 frames have passed since start, clear the flames
-            if (current_frame - flame_start_frames[row] >= 10)
-            {
+            if (current_frame - flame_start_frames[row] >= 10) {
                 clear_flames_on_row(row);
                 flame_active[row] = 0;
 
                 // Restore the background for the entire row
-                for (int col = 0; col < GRID_COLS; col++)
-                {
+                for (int col = 0; col < GRID_COLS; col++) {
                     int x, y;
                     grid_to_pixel(col, row, &x, &y);
                     restore_background_area(x, y, GRID_COL_WIDTH, GRID_ROW_HEIGHT, 0);
                 }
-            }
-            else
-            {
+            } else {
                 draw_flames_on_row(row);
             }
         }
     }
 }
 
-void apply_chilli_damage(Zombie *zombie)
-{
+void apply_chilli_damage(Zombie *zombie) {
     if (!zombie->active)
         return;
 
@@ -338,42 +310,38 @@ void apply_chilli_damage(Zombie *zombie)
 }
 
 // get plant name
-const char *get_plant_name(int type)
-{
-    switch (type)
-    {
-    case PLANT_TYPE_SUNFLOWER:
-        return "Sunflower";
-    case PLANT_TYPE_PEASHOOTER:
-        return "Peashooter";
-    case PLANT_TYPE_FROZEN_PEASHOOTER:
-        return "Snow Pea";
-    case PLANT_TYPE_WALLNUT:
-        return "Wall-nut";
-    case PLANT_TYPE_CHILLIES:
-        return "Chilies";
-    case SHOVEL:
-        return "Shovel";
-    default:
-        return "Unknown";
+const char *get_plant_name(int type) {
+    switch (type) {
+        case PLANT_TYPE_SUNFLOWER:
+            return "Sunflower";
+        case PLANT_TYPE_PEASHOOTER:
+            return "Peashooter";
+        case PLANT_TYPE_FROZEN_PEASHOOTER:
+            return "Snow Pea";
+        case PLANT_TYPE_WALLNUT:
+            return "Wall-nut";
+        case PLANT_TYPE_CHILLIES:
+            return "Chilies";
+        case SHOVEL:
+            return "Shovel";
+        default:
+            return "Unknown";
     }
 }
 
-int get_plant_cost(int plant_type)
-{
-    switch (plant_type)
-    {
-    case PLANT_SUNFLOWER:
-        return default_sunflower.cost;
-    case PLANT_PEASHOOTER:
-        return default_peashooter.cost;
-    case PLANT_FROZEN_PEASHOOTER:
-        return default_frozen_peashooter.cost;
-    case PLANT_WALNUT:
-        return default_walnut.cost;
-    case PLANT_CHILLIES:
-        return default_chillies.cost;
-    default:
-        return 0;
+int get_plant_cost(int plant_type) {
+    switch (plant_type) {
+        case PLANT_SUNFLOWER:
+            return default_sunflower.cost;
+        case PLANT_PEASHOOTER:
+            return default_peashooter.cost;
+        case PLANT_FROZEN_PEASHOOTER:
+            return default_frozen_peashooter.cost;
+        case PLANT_WALNUT:
+            return default_walnut.cost;
+        case PLANT_CHILLIES:
+            return default_chillies.cost;
+        default:
+            return 0;
     }
 }
