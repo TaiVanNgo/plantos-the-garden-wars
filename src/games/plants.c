78179353872a5afe909254d89581447bc2258c04
Thu Nguyen <101914949+npat273@users.noreply.github.com>
2025-05-27 12:32:57 +0700

    fix(plant): fix game freezes after placing plants

--- src/games/plants.c
+++ src/games/plants.c
@@ -1,6 +1,8 @@
-#include "plants.h"
+#include "../../include/plants.h"
 
-#include "game_init.h"
+#include "../../include/game_init.h"
+#include "../../include/grid.h"
+#include "../../include/zombies.h"
 
 int flame_start_frames[GRID_ROWS] = {0};
 int flame_active[GRID_ROWS] = {0};
@@ -14,6 +16,7 @@ const Plant default_sunflower = {
     .row = 0,
     .attack_damage = 0,  // Sunflowers don't attack
     .cost = 50,
+    .attack_speed = 0,  // Sunflowers don't attack
 };
 
 // Default Peashooter
@@ -25,6 +28,7 @@ const Plant default_peashooter = {
     .row = 0,
     .attack_damage = 30,
     .cost = 100,
+    .attack_speed = 5,  // Fires 5 peas per time unit
 };
 
 // Default Frozen Peashooter
@@ -36,6 +40,7 @@ const Plant default_frozen_peashooter = {
     .row = 0,
     .attack_damage = 20,  // Slightly less damage than regular peashooter
     .cost = 175,          // More expensive due to freezing ability
+    .attack_speed = 4,    // Fires 4 peas per time unit
 };
 
 // Default Walnut
@@ -47,6 +52,7 @@ const Plant default_walnut = {
     .row = 0,
     .attack_damage = 0,  // Walnuts don't attack
     .cost = 50,
+    .attack_speed = 0,  // Walnuts don't attack
 };
 
 // Default Chillies
@@ -58,6 +64,7 @@ const Plant default_chillies = {
     .row = 0,
     .attack_damage = 255,  // High damage as it's an explosive plant
     .cost = 125,
+    .attack_speed = 1,
 };
 
 // Function to create a new plant of the specified type
